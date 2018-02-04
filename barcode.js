da1f1de189f8b83878892e740fd21a280a2f034b
Tom Penney <dev@2kan.io>
2018-02-04 13:52:12 +1100

    Added more colors and start of reed-solomon error correction

--- barcode.js
+++ barcode.js
@@ -29,11 +29,11 @@ const barSpec = {
 	"3": { top: 0.5, bottom: 0.5 }
 }
 
-const encoded = Encode( ENCODING.C, "I hope this isnt copyrighted by Australia Post" );
+const encoded = Encode( ENCODING.C, "I'm fairly certain this isnt copyrighted by Australia Post" );
 console.log( encoded );
 
-Write( encoded, undefined, colors.PINK );
-console.log( Encode( ENCODING.C, "purple is a fruit" ) );
+Write( encoded, undefined, colors.PURPLE );
+//console.log( Encode( ENCODING.C, "purple is a fruit" ) );
 
 function CreateAusPostBarcode( a_formatType, a_format, a_DPID, a_customerInfo )
 {
@@ -143,4 +143,56 @@ function DrawRect( a_image, a_startX, a_startY, a_endX, a_endY, a_color )
 	for ( var x = a_startX; x < a_endX; ++x )
 		for ( var y = a_startY; y < a_endY; ++y )
 			a_image.setAt( x, y, a_color );
+}
+
+
+// Reed solomon error correction code (WIP)
+var mult = [ [], [] ];
+var gen = [ 48, 17, 29, 30, 1 ];
+function RSInit()
+{
+	var primpoly = 67;
+	var test = 64;
+
+	for ( var i = 0; i < 64; ++i )
+	{
+		mult[ 0 ].push( 0 );
+		mult[ 1 ].push( i );
+	}
+
+	var next;
+	var prev = 1;
+	for ( var i = 1; i < 64; ++i )
+	{
+		next = prev << 1;
+		if ( next & test )
+			next ^= primpoly;
+
+		for ( var k = 0; k < 64; ++k )
+		{
+			mult[ next ][ k ] = mult[ prev ][ k ] << 1;
+			if ( mult[ next ][ k ] & test )
+				mult[ next ][ k ] ^= primpoly;
+		}
+
+		prev = next;
+	}
+}
+
+function RSEncode( a_infosym, a_infosymbols )
+{
+	var temp = [];
+
+	var paritystring = [];
+
+	var k = a_infosym;
+	var n = k + 4;
+
+	var i = 0;
+	for ( i = 0; i < 4; ++i )
+	{
+		temp[ i ] = 0;
+		for ( i = 4; i < n; ++i )
+			temp[ i ] = a_infosymbols[ i - 4 ];
+	}
 }
